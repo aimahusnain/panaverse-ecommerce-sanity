18a4091e88878e0192bb9e1dbfab6e691b319837
aimahusnain <aimahusnain@gmail.com>
2024-01-30 01:47:50 +0500

    new one

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,7 @@ const getProductionData = async () => {
     _id,
     title,
     image,
-    category -> {
-    name
-    }
+    category -> {name}
   }`);
   return res;
 };
@@ -31,20 +29,24 @@ export default async function Home() {
   const data: IProduct[] = await getProductionData();
 
   return (
-    <div className="grid grid-cols-[repeat, auto] justify-center gap-x-10">
+    <div className="text-white grid grid-cols-[repeat, auto] justify-center gap-x-10">
+      
       {data.map((item, i) => (
-          <div key={i}>
-            <Image
-              width={200}
-              height={300}
-              className="max-h-[200px] object-cover object-top"
-              src={urlForImage(item.image).url()}
-              alt="product"
+        
+        <div key={i}>
+          <Image
+            width={200}
+            height={300}
+            className="max-h-[200px] object-cover object-top"
+            src={urlForImage(item.image).url()}
+            alt="product"
             />
-            <h1>{item.title}</h1>
-            <h3>${item.price}</h3>
-            <button className="border px-6 py-2 rounded bg-blue-600 text-white">Add to Cart</button>            
-          </div>
+          <h1>{item.title}</h1>
+          <h3>${item.price}</h3>
+          <button className="border px-6 py-2 rounded bg-blue-600 text-white">
+            Add to Cart
+          </button>
+        </div>
       ))}
     </div>
   );
